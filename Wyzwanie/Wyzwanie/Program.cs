f2e0d726b5473eedceeecd659a2edccaf22cb423
Tomasz Kondro <tkondro@gmail.com>
2024-01-11 00:02:26 +0100

    Zmienne

---
+++ Wyzwanie/Wyzwanie/Program.cs
@@ -0,0 +1,28 @@
+﻿//var myAge = 40;
+
+using System.Diagnostics.CodeAnalysis;
+
+int myAge = 120;
+int newAge = myAge + 5;
+//Console.WriteLine(newAge);
+int myvar = int.MinValue;
+uint myvar2 = uint.MaxValue;
+ulong myVar3 = ulong.MaxValue;
+
+// Liczba Zmiennoprzecinkowa;
+float myNumber = float.MaxValue;
+double myNumber2 = double.MaxValue;
+
+//Zmienna Tekstowa;
+string name = "Tomasz";
+string surname = "Kondro";
+string result = name + surname + myAge;
+//Console.WriteLine(result);
+char myVar5 = 'c';
+var result2 = name.ToArray();
+
+// Zmienna Logiczna;
+bool isActive = true;
+isActive = false;
+var isValid = 5 > 6;
+Console.WriteLine(isValid);
